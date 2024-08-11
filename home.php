5a0fc6e567ee1321e7da8f5548cdb398a1249d40
Vinul Vinsura <vinulviksura16@gmail.com>
2024-08-11 19:14:44 +0530

    impliment home page

--- home.php
+++ home.php
@@ -1,4 +1,410 @@
-<?php
- echo "success entering..";
+<!DOCTYPE html>
+<html lang="en">
 
-?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sprinkle Delight</title>
+
+    <!-- swiper link  -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+
+    <!-- cdn icon link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <!-- custom css file  -->
+    <link rel="stylesheet" href="home.css">
+
+</head>
+
+<body>
+
+    <!-- header section start here  -->
+    <header class="header">
+        <div class="logoContent">
+            <a href="#" class="logo"><img src="images1/logo1.png" alt=""></a>
+            <h1 class="logoName">Sprinkle Delight</h1>
+        </div>
+
+        <nav class="navbar">
+            <a href="#home">home</a>
+            <a href="#product">product</a>
+            <a href="#blogs">blogs</a>
+            <a href="#review">review</a>
+            <a href="#contact">contact</a>
+
+        </nav>
+
+        <div class="icon">
+            <i class="fas fa-search" id="search"></i>
+            <i class="fas fa-bars" id="menu-bar"></i>
+        </div>
+
+        <div class="search">
+            <input type="search" placeholder="search...">
+        </div>
+    </header>
+    <!-- header section end here  -->
+
+    <!-- home section start here  -->
+    <section class="home" id="home">
+        <div class="homeContent">
+            <h2>Tasty Cup Cakes for Everyone </h2>
+            <p>Delight in our freshly baked, gourmet cupcakes made with the finest ingredients. From classic flavors to custom creations, we offer a sweet treat for every occasion. 
+                Indulge in the perfect blend of taste and artistry—order your favorite cupcakes today</p>
+            <div class="home-btn">
+                <a href="#"><button>see more</button></a>
+            </div>
+        </div>
+    </section>
+
+    <!-- home section end here  -->
+
+    <!-- product section start here  -->
+    <section class="product" id="product">
+        <div class="heading">
+            <h2>Our Exclusive Products</h2>
+        </div>
+        <div class="swiper product-row">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide box">
+                    <div class="img">
+                        <img src="images1/Decadent.png" alt="">
+                    </div>
+                    <div class="product-content">
+                        <h3>Decadent Dark Chocolate Truffle</h3>
+                        <p>A rich dark chocolate cupcake filled with creamy truffle ganache and topped with silky chocolate buttercream
+                            <span class="price"><h3><strong>Rs.600.00</strong></h3></span>
+                        </p>
+                       
+                        <div class="orderNow">
+                            <a href="indexorder.html"><button>Order now</button></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide box">
+                    <div class="img">
+                        <img src="images1/salt.png" alt="">
+                    </div>
+                    <div class="product-content">
+                        <h3>Salted Caramel Mocha Delight</h3>
+                        <p>A chocolate cupcake infused with espresso, filled with gooey salted caramel, and crowned with mocha frosting and a caramel drizzle.
+                        </p>
+                        <span class="price"><h3><strong>Rs.550.00</strong></h3></span>
+                        <div class="orderNow">
+                            <a href="indexorder.html"><button>Order now</button></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide box">
+                    <div class="img">
+                        <img src="images1/rasp.png" alt="">
+                    </div>
+                    <div class="product-content">
+                        <h3>Raspberry Chocolate Bliss</h3>
+                        <p>A moist chocolate cupcake with a tart raspberry filling, topped with raspberry-infused chocolate ganache.
+                            <span class="price"><h3><strong>Rs.620.00</strong></h3></span>
+                        </p>
+                        <div class="orderNow">
+                            <a href="indexorder.html"><button>Order now</button></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide box">
+                    <div class="img">
+                        <img src="images1/peanut.png" alt="">
+                    </div>
+                    <div class="product-content">
+                        <h3>Peanut Butter Fudge Cupcake</h3>
+                        <p>A chocolate cupcake with a smooth peanut butter center, topped with fudge frosting and a sprinkle of crushed peanuts.
+                            <span class="price"><h3><strong>Rs.700.00</strong></h3></span>
+                        </p>
+                        <div class="orderNow">
+                            <a href="indexorder.html"><button>Order now</button></a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="swiper-pagination"></div>
+        </div>
+        <div class="swiper product-row">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide box">
+                    <div class="img">
+                        <img src="images1/double.png" alt="">
+                    </div>
+                    <div class="product-content">
+                        <h3>Double Chocolate Mint</h3>
+                        <p>A chocolate cupcake with a refreshing mint cream filling, topped with dark chocolate frosting and a sprinkle of mint chips.
+                            <span class="price"><h3><strong>Rs.750.00</strong></h3></span>
+                        </p>
+                        <div class="orderNow">
+                            <a href="indexorder.html"><button>Order now</button></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide box">
+                    <div class="img">
+                        <img src="images1/white.png" alt="">
+                    </div>
+                    <div class="product-content">
+                        <h3>White Chocolate Raspberry Swirl</h3>
+                        <p>A chocolate cupcake swirled with white chocolate and raspberry, topped with white chocolate buttercream and a raspberry garnish.
+                            <span class="price"><h3><strong>Rs.730.00</strong></h3></span>
+                        </p>
+                        <div class="orderNow">
+                            <a href="indexorder.html"><button>Order now</button></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide box">
+                    <div class="img">
+                        <img src="images1/hazel.png" alt="">
+                    </div>
+                    <div class="product-content">
+                        <h3>Chocolate Hazelnut Dream
+                        </h3>
+                        <p>A rich chocolate cupcake filled with a creamy hazelnut spread, topped with chocolate-hazelnut buttercream and a sprinkle of crushed hazelnuts.
+                            <span class="price"><h3><strong>Rs.550.00</strong></h3></span>
+                        </p>
+                        <div class="orderNow">
+                            <a href="indexorder.html"><button>Order now</button></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide box">
+                    <div class="img">
+                        <img src="images1/crunch.png" alt="">
+                    </div>
+                    <div class="product-content">
+                        <h3>Cookies and Cream Crunch</h3>
+                        <p>A chocolate cupcake with a cookies and cream filling,and cream buttercream and crushed chocolate sandwich cookies.
+                            <span class="price"><h3><strong>Rs.450.00</strong></h3></span>
+                        </p>
+                        <div class="orderNow">
+                            <a href="indexorder.html"><button>Order now</button></a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="swiper-pagination"></div>
+        </div>
+    </section>
+
+    <!-- product section end here  -->
+
+    <!-- blogs section start here  -->
+    <section class="blogs" id="blogs">
+
+        <div class=" swiper blogs-row">
+            <div class="swiper-wrapper">
+                <div class=" swiper-slide box">
+                    <div class="img">
+                        <img src="images1/blog1.png" alt="">
+                    </div>
+                    <div class="content">
+                        <h3>The Sweet Story Behind Our Cupcakes </h3>
+                       <p>At Sweet Cravings, every cupcake tells a story. Our journey began with a passion for baking and a desire to create something truly special.
+                         Each of our cupcakes is handcrafted with love, using only the finest ingredients. From the rich, velvety chocolate to the fresh, tangy fruits, every bite is a celebration of flavor. 
+                         We believe that a cupcake is more than just a dessert—it's a moment of joy, a little piece of happiness that you can share with others or savor all to yourself. 
+                        Join us on our sweet adventure, one cupcake at a time</p>
+
+                       <a href="#blogs" class="btn">learn more</a>
+                    </div>
+                </div>
+                <div class=" swiper-slide box">
+                    <div class="img">
+                        <img src="images1/blog2.png" alt="">
+                    </div>
+                    <div class="content">
+                        <h3>Why Quality Ingredients Matter </h3>
+                        <p>When it comes to making the perfect cupcake, we believe that quality is key. That’s why we source only the best ingredients for our recipes. 
+                            From organic vanilla beans to locally sourced dairy, every component of our cupcakes is carefully selected to ensure the best possible taste. 
+                            We never cut corners because we know that great ingredients make great cupcakes. 
+                            It’s this commitment to quality that sets us apart, and it’s why every bite of our cupcakes is pure bliss.</p>
+                        <a href="#blogs" class="btn">learn more</a>
+                    </div>
+                </div>
+                <div class=" swiper-slide box">
+                    <div class="img">
+                        <img src="images1/blog3.png" alt="">
+                    </div>
+                    <div class="content">
+                        <h3>Our Most Popular Flavors : A Must Try Experience </h3>
+                        <p>Our customers have spoken, and these are the flavors they just can't get enough of! The Decadent Dark Chocolate Truffle is a rich indulgence, filled with silky ganache and topped with smooth buttercream. 
+                            For those who love a little zest, our Lemon Berry Delight offers a refreshing burst of flavor with every bite. And let’s not forget the Salted Caramel Mocha, a perfect blend of sweet and savory with a touch of coffee.
+                             Whether you’re new to Sweet Cravings or a loyal fan, these popular flavors are a must-try!</p>
+                        <a href="#blogs" class="btn">learn more</a>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-pagination"></div>
+
+
+        </div>
+    </section>
+
+    <!-- blogs section ends here  -->
+
+    <!-- newsletter section start here  -->
+
+    <section class="newsletter">
+        <form action="">
+            <h3>subscribe for latest update</h3>
+            <input type="email" name="" placeholder="enter your email" id="" class="box">
+            <input type="button" value="subscribe" class="box2">
+        </form>
+    </section>
+    <!-- newsletter section ends here  -->
+
+    <!-- review section start here  -->
+    <section class="review" id="review">
+        <div class="heading">
+            <h2>client review</h2>
+        </div>
+
+
+        <div class=" swiper review-row">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide box">
+                    <div class="client-review">
+                        <p>Sweet Cravings never disappoints. The Triple Chocolate Overload is a chocolate lover's dream come true. 
+                            Rich, moist, and absolutely decadent!</p>
+                    </div>
+                    <div class="client-info">
+                        <div class="img">
+                            <img src="images1/c1.png" alt="">
+                        </div>
+                        <div class="clientDetailed">
+                            <h3>Kalana Dilshan</h3>
+                            <p>University Student</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide box">
+                    <div class="client-review">
+                        <p>I ordered the Salted Caramel Mocha Delight for a party, and they were a huge hit.
+                             Everyone asked where they were from. Definitely ordering again!</p>
+                    </div>
+                    <div class="client-info">
+                        <div class="img">
+                            <img src="images1/c2.png" alt="">
+                        </div>
+                        <div class="clientDetailed">
+                            <h3>Didula Heshan</h3>
+                            <p>Graphic Designer</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide box">
+                    <div class="client-review">
+                        <p>The custom Raspberry Chocolate Bliss cupcakes were not only beautiful but also incredibly delicious. 
+                            Sweet Cravings made our special day even sweeter!</p>
+                    </div>
+                    <div class="client-info">
+                        <div class="img">
+                            <img src="images1/c3.png" alt="">
+                        </div>
+                        <div class="clientDetailed">
+                            <h3>Vinul Viksura</h3>
+                            <p>Software engineer at IFS</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide box">
+                    <div class="client-review">
+                        <p>I tried the White Chocolate Raspberry Swirl, and it was a game-changer.
+                             The perfect balance of sweet and chocolate—I’ve never tasted anything like it!</p>
+                    </div>
+                    <div class="client-info">
+                        <div class="img">
+                            <img src="images1/c4.png" alt="">
+                        </div>
+                        <div class="clientDetailed">
+                            <h3>Devinda Dilshan</h3>
+                            <p>Data Scientist at Anzell</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-pagination"></div>
+
+            </div>
+        </div>
+    </section>
+    <!-- review section ends here  -->
+
+    <!-- footer section start here  -->
+
+    <footer class="footer" id="contact">
+        <div class="box-container">
+            <div class="mainBox">
+                <div class="content">
+                    <a href="#"><img src="images1/logo1.png" alt=""></a>
+                    <h1 class="logoName"> Sprinkle Delight </h1>
+                </div>
+
+                <p>Delight in our exquisite selection of handcrafted cupcakes and cakes, made with love and the finest ingredients. 
+                    Whether you're celebrating a special occasion or just treating yourself, 
+                    our exclusive products are designed to bring joy to every moment.</p>
+
+            </div>
+            <div class="box">
+                <h3>Quick link</h3>
+                <a href="#"> <i class="fas fa-arrow-right"></i>Home</a>
+                <a href="#"> <i class="fas fa-arrow-right"></i>product</a>
+                <a href="#"> <i class="fas fa-arrow-right"></i>blogs</a>
+                <a href="#"> <i class="fas fa-arrow-right"></i>review</a>
+                <a href="#"> <i class="fas fa-arrow-right"></i>contact</a>
+
+            </div>
+            <div class="box">
+                <h3>Extra link</h3>
+                <a href="#"> <i class="fas fa-arrow-right"></i>Create an Account</a>
+                <a href="indexorder.html"> <i class="fas fa-arrow-right"></i>order item</a>
+                <a href="#"> <i class="fas fa-arrow-right"></i>privacy policy</a>
+                <a href="#"> <i class="fas fa-arrow-right"></i>payment method</a>
+                <a href="#"> <i class="fas fa-arrow-right"></i>our  services</a>
+            </div>
+            <div class="box">
+                <h3>Contact Info</h3>
+                <a href="#"> <i class="fas fa-phone"></i>035-8976542</a>
+                <a href="#"> <i class="fas fa-envelope"></i>Sprinkle Delight@gmail.com</a>
+
+            </div>
+
+        </div>
+        <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+            <a href="#" class="fab fa-pinterest"></a>
+        </div>
+        <div class="credit">
+            created by <span>Group 8 Students </span> |all rights reserved ! 
+        </div>
+    </footer>
+
+
+
+
+
+
+
+
+
+
+    <!-- swiper js link  -->
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+    <!-- custom js file  -->
+    <script src="home.js"></script>
+
+
+</body>
+
+</html>
